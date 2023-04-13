3b2921700a940783e2b0d2ecf5f9d12c796e17a5
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-04-13 23:49:37 +0200

    added test WebView and test site

--- src/main/java/com/arek/controllers/GrammarTabController.java
+++ src/main/java/com/arek/controllers/GrammarTabController.java
@@ -3,18 +3,32 @@ package com.arek.controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
+import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Logger;
 
 public class GrammarTabController implements Initializable {
 
-    @FXML private ScrollPane sitePane;
+    @FXML private WebView browser;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        File siteFile = new File(".\\sites\\test.html");
 
-        //sitePane.setContent(new Web);
+        try{
+            browser.getEngine().load(siteFile.toURI().toURL().toString());
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+
+        //browser.getEngine().loadContent("<b>asdf</b>");
+
     }
 }
