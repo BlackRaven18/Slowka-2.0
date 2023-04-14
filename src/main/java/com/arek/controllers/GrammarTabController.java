332d2cd820dc179378db656f4768777dc61e6901
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-04-14 21:38:30 +0200

    improved grammar site

--- src/main/java/com/arek/controllers/GrammarTabController.java
+++ src/main/java/com/arek/controllers/GrammarTabController.java
@@ -2,7 +2,9 @@ package com.arek.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Accordion;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TitledPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
@@ -10,17 +12,18 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Logger;
+
 
 public class GrammarTabController implements Initializable {
 
     @FXML private WebView browser;
 
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        File siteFile = new File(".\\sites\\test.html");
+        File siteFile = new File(".\\sites\\index.html");
 
         try{
             browser.getEngine().load(siteFile.toURI().toURL().toString());
@@ -28,7 +31,6 @@ public class GrammarTabController implements Initializable {
             e.printStackTrace();
         }
 
-        //browser.getEngine().loadContent("<b>asdf</b>");
 
     }
 }
